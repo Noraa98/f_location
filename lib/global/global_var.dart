598223f91e241fd7e65c6e63179351bcea9f0b2e
Noraa98 <badr91780@gmail.com>
2024-04-07 14:15:30 +0200

    initial commit

---
+++ lib/global/global_var.dart
@@ -0,0 +1,9 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+String googleMapKey = "AIzaSyCnf5vmISVW0pB3XbiyM9wsACG85GuyIqI";
+
+
+const CameraPosition googlePlexInitialPosition = CameraPosition(
+    target: LatLng(31, 41),
+    zoom: 14.4746
+);
